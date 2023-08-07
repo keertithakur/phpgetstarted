80060cd0b184811b1daf1c028661f7df8457e115
keertithakur <keerti.strixa@gmailcom>
2023-08-07 15:30:15 +0530

    test

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php phpinfo(); ?>
 
-<?php echo 'terminal' ?>
+<?php echo 'terminal' 
+
+$a =array("computer memory"=>
+                            ["primary memory"=>
+                                                ["RAM"=>["SRAM"
+                                                         ,"DRAM"
+                                                        ]
+                                                ,"ROM"=>["PROM"
+                                                        ,"EPROM"
+                                                        ,"EEPROM"
+                                                        ]
+                                                 ]             //multidimension array
+                              ,"secondry memory"=>
+                                                   ["magnetic storage"=> ["HDD"
+                                                                         ,"floppy disk"
+                                                                         ,"magnetic tape"
+                                                                         ]
+                                                     ,"optical storage"=>["CD"
+                                                                          ,"DVD"
+                                                                         ]
+                                                     ,"flash storage"=>["pen drive"
+                                                                         ,"memory card"
+                                                                        ]
+                                                    ]
+                             ]
+);
+print_r($a);
+
+?>
